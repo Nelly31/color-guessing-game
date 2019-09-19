e5d208332060f11372219af85bec34cc4f6f627b
Nelly <nelly_main@hotmail.com>
2019-09-19 18:03:58 -0400

    add easy and hard modes

--- color-guesser.js
+++ color-guesser.js
@@ -1,13 +1,60 @@
-let colors = generateRandomColors(6);
-
-
+let numSquares = 6
+let colors = generateRandomColors(numSquares);
 let squares = document.querySelectorAll(".square");
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay")
 let messageDisplay = document.querySelector("#message");
 let h1 = document.querySelector("h1")
+let resetButton = document.querySelector("#reset")
+let easyButton = document.querySelector("#easyBtn")
+let hardButton = document.querySelector("#hardBtn")
 
-colorDisplay.textContent = pickedColor;
+
+easyButton.addEventListener("click", function() {
+  easyButton.classList.add("selected");
+  hardButton.classList.remove("selected")
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  h1.style.backgroundColor = "#232323";
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor.toUpperCase();
+  for (let i = 0; i <squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+})
+
+hardButton.addEventListener("click", function() {
+  hardButton.classList.add("selected")
+  easyButton.classList.remove("selected")
+  numSquares = 6
+  colors = generateRandomColors(numSquares);
+  h1.style.backgroundColor = "#232323";
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor.toUpperCase();
+  for (let i = 0; i <squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
+})
+
+resetButton.addEventListener("click", function(){
+  numSquares = 6
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor.toUpperCase();
+
+  for (let i = 0; i < squares.length; i++) {
+    // add initial colors to squares
+    squares[i].style.backgroundColor = colors[i]
+  }
+  h1.style.backgroundColor = "#232323";
+});
+
+colorDisplay.textContent = pickedColor.toUpperCase();
 
 for (let i = 0; i < squares.length; i++) {
   // add initial colors to squares
@@ -18,6 +65,7 @@ for (let i = 0; i < squares.length; i++) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!"
       changeColors(clickedColor);
+      resetButton.textContent = "Play Again?"
       h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
